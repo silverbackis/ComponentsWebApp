acd989a918b9ee882c4a29eefba4f1180efef5bd
Daniel West <daniel@silverback.is>
2018-01-04 00:20:20 +0000

    /bin/bash scripts

--- docker-dev.sh
+++ docker-dev.sh
@@ -1,2 +1,2 @@
-#!bin/bash
+#bin/bash
 docker-compose -f docker-compose.yaml -f docker-compose-dev.yaml up -d
--- docker-prod.sh
+++ docker-prod.sh
@@ -1,2 +1,2 @@
-#!bin/bash
+#bin/bash
 docker-compose -f docker-compose.yaml -f docker-compose-prod.yaml up -d --force-recreate
