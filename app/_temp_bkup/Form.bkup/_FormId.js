f93a8d130134992abf542fde7b23ccaa657b8b00
Daniel West <daniel@silverback.is>
2018-01-08 21:01:15 +0000

    Remove old form backups

--- app/_temp_bkup/Form.bkup/_FormId.js
+++
@@ -1,7 +0,0 @@
-export default {
-  computed: {
-    formId () {
-      return 'form_' + this.form.vars.id || this.form.vars.attr.id
-    }
-  }
-}
--- app/_temp_bkup/forms.js
+++
@@ -1,9 +0,0 @@
-import { mutations } from './form/mutations'
-import { getters } from './form/getters'
-import { actions } from './form/actions'
-
-export const state = () => ({
-  forms: {}
-})
-
-export { getters, mutations, actions }
