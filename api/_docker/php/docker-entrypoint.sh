d4c0127300925cc1c4b77406d897657ed4dcc404
Daniel West <daniel@silverback.is>
2018-04-17 11:48:20 +0100

    Create public/media with correct permissions

--- api/_docker/php/docker-entrypoint.sh
+++ api/_docker/php/docker-entrypoint.sh
@@ -7,7 +7,7 @@ if [ "${1#-}" != "$1" ]; then
 fi
 
 if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
-  mkdir -p var/cache var/log var/sessions var/logs
+  mkdir -p var/cache var/sessions var/logs public/media
 	composer install --prefer-dist --no-progress --no-suggest --no-interaction
 
 	if [ "$APP_ENV" != 'prod' ]; then
@@ -15,12 +15,13 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
 		bin/console c:c
   else
     composer run-script --no-dev post-install-cmd
+    bin/console assets:install
 		bin/console c:c
     bin/console c:w
 	fi
 
 	# Permissions hack because setfacl does not work on Mac and Windows
-	chown -R www-data var
+	chown -R www-data var public/media
 fi
 
 exec docker-php-entrypoint "$@"
