dbbc8fdab7330085d6dd75a5ed51b0584a1b5d47
Daniel West <daniel@silverback.is>
2018-08-29 16:02:10 +0100

    Sortable gallery with buttons

--- app/plugins/fontawesome.js
+++ app/plugins/fontawesome.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub'
-import { faBook, faBars, faExclamationTriangle, faCheck, faCheckCircle } from '@fortawesome/free-solid-svg-icons'
+import { faBook, faBars, faExclamationTriangle, faCheck, faCheckCircle, faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faGithub, faCheckCircle, faBook, faCheck, faExclamationTriangle, faBars)
+library.add(faGithub, faCheckCircle, faBook, faCheck, faExclamationTriangle, faBars, faChevronUp, faChevronDown)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
