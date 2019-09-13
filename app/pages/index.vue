512d5add9a66e06851c262077bf9e75f6613ed55
Daniel West <daniel@silverback.is>
2019-09-13 08:54:14 +0100

    Add default 'index' page into front-end app

---
+++ app/pages/index.vue
@@ -0,0 +1,12 @@
+<script>
+import BasePage from '~/.nuxt/bwstarter/bulma/pages/_base.vue'
+export default {
+  extends: BasePage,
+  head() {
+    return Object.assign({}, this.headData, {
+      titleTemplate:
+        'Components Web App - Demo Site'
+    })
+  }
+}
+</script>
