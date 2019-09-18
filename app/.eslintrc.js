f90ddcdeee5d6d4a1fe70b5c450fa825e546e036
Daniel West <daniel@silverback.is>
2019-09-18 13:00:14 +0100

    eslint, prettier and yarn updates/upgrades

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -8,17 +8,17 @@ module.exports = {
     parser: 'babel-eslint'
   },
   extends: [
-    'plugin:vue/recommended',
-    'plugin:prettier/recommended'
+    '@nuxtjs',
+    'plugin:nuxt/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+    'prettier/vue'
   ],
-  // required to lint *.vue files
   plugins: [
-    'vue',
     'prettier'
   ],
   // add your custom rules here
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'nuxt/no-cjs-in-config': 'off'
   }
 }
