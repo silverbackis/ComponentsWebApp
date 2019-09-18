5619137f81892580be41f3a0b28bb7313ffa5265
Daniel West <daniel@silverback.is>
2019-09-18 11:15:08 +0100

    Update ACB

--- app/plugins/fontawesome.js
+++ app/plugins/fontawesome.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 // import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub'
-import { faCheck, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
+import { faCheck, faExclamationTriangle, faSave, faEdit, faSync } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faCheck, faExclamationTriangle)
+library.add(faCheck, faExclamationTriangle, faSave, faEdit, faSync)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
