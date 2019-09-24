c83c75e66efb0a1057cb1d7eb1b42b178dfa7eec
Daniel West <daniel@silverback.is>
2019-09-24 20:13:03 +0100

    Add trash FA icon for collections and tag bulma css for 'published' indicator

--- app/plugins/fontawesome.js
+++ app/plugins/fontawesome.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 // import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub'
-import { faCheck, faExclamationTriangle, faSave, faEdit, faSync } from '@fortawesome/free-solid-svg-icons'
+import { faCheck, faExclamationTriangle, faSave, faEdit, faSync, faTrash } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faCheck, faExclamationTriangle, faSave, faEdit, faSync)
+library.add(faCheck, faExclamationTriangle, faSave, faEdit, faSync, faTrash)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
