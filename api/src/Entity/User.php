374aad8b6f9e80ab5b6167b203ade130e6104e00
Daniel West <daniel@silverback.is>
2020-04-07 18:41:20 +0100

    Updates to make in-line with latest ACB updates

--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 use Silverback\ApiComponentBundle\Entity\User\User as BaseUser;
 
 /**
+ * @ApiResource(attributes={"security"="is_granted('ROLE_ADMIN') or object.owner == user"})
  * @ORM\Entity()
  * @ORM\Table(name="`user`")
  */
